29efd8d3d53e1cc631ee7bd88fdeaca541b94010
Alexander Kholodovitch <kholodovitch@gmail.com>
2012-04-09 22:46:49 +0000

    remove test-logic for creating temporary graph - load graph from file

--- src/OpencvGraphEdit/MainForm.Designer.cs
+++ src/OpencvGraphEdit/MainForm.Designer.cs
@@ -28,17 +28,41 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
 			this.graphControl1 = new Visualizer.GraphControl();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.toolStrip1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
 			// 
 			// graphControl1
 			// 
 			this.graphControl1.BackColor = System.Drawing.SystemColors.Window;
 			this.graphControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.graphControl1.Location = new System.Drawing.Point(0, 0);
+			this.graphControl1.Location = new System.Drawing.Point(0, 25);
 			this.graphControl1.Name = "graphControl1";
-			this.graphControl1.Size = new System.Drawing.Size(784, 562);
-			this.graphControl1.TabIndex = 0;
+			this.graphControl1.Size = new System.Drawing.Size(784, 537);
+			this.graphControl1.TabIndex = 2;
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton1.Text = "toolStripButton1";
+			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
 			// 
 			// MainForm
 			// 
@@ -46,16 +70,22 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(784, 562);
 			this.Controls.Add(this.graphControl1);
+			this.Controls.Add(this.toolStrip1);
 			this.Name = "MainForm";
 			this.Text = "MainForm";
 			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnFormClosing);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
+		private System.Windows.Forms.ToolStrip toolStrip1;
 		private Visualizer.GraphControl graphControl1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
 	}
 }
 
--- src/OpencvGraphEdit/MainForm.cs
+++ src/OpencvGraphEdit/MainForm.cs
@@ -1,89 +1,35 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 using System.Windows.Forms;
-using FilterImplementation;
 using FilterImplementation.Base;
-using FilterImplementation.Destination;
 using FilterImplementation.Serialization;
-using FilterImplementation.Source;
 
 namespace OpencvGraphEdit
 {
 	public partial class MainForm : Form
 	{
-		const string BlurName = "FilterImplementation.Filters.Image.Blur";
-		const string CloneName = "FilterImplementation.Filters.Image.Clone";
+		private const string PathToXml = @"..\..\sample_graph.xml";
 
 		public MainForm()
 		{
 			InitializeComponent();
 
-			var graph = CreateTestGraph();
-			foreach (Filter filter in graph.Filters)
-			{
-				Filter localFilter = filter;
-				ThreadPool.QueueUserWorkItem(o => localFilter.Process());
-			}
-
-			var graphBundle = new GraphBundle(graph);
-			GraphSaver.Save(graphBundle, "graph0.xml", SaveOptions.AddComments);
-			graphBundle = GraphLoader.Load("graph1.xml");
-			//GraphSaver.Save(graph, "graph1.xml", SaveOptions.AddComments);
+			var graphBundle = GraphLoader.Load(PathToXml);
 			graphControl1.LoadGraph(graphBundle);
 		}
 
 		private void OnFormClosing(object sender, FormClosingEventArgs e)
 		{
-			GraphSaver.Save(graphControl1.GraphBundle, "graph1.xml", SaveOptions.AddComments);
+			GraphSaver.Save(graphControl1.GraphBundle, PathToXml, SaveOptions.AddComments);
 		}
 
-		private static Graph CreateTestGraph()
+		private void toolStripButton1_Click(object sender, EventArgs e)
 		{
-			var graph = new Graph();
-			var sourceFileImage = new SourceFileImage();
-			sourceFileImage.Name = "FileImage";
-			sourceFileImage.Properties.Values.First().Value = @"input.png";
-			graph.AddFilter(sourceFileImage);
-
-			var assembly = FiltersHelper.GetFilterTypes().First().Assembly;
-			var clone = (Filter) assembly.CreateInstance(CloneName);
-			Debug.Assert(clone != null);
-			clone.Properties.Values.First().Value = 1;
-			sourceFileImage.OutputPins.First().Connect(clone.InputPins.First());
-			graph.AddFilter(clone);
-
-			var filter0_0 = (Filter) assembly.CreateInstance(BlurName);
-			Debug.Assert(filter0_0 != null);
-			clone.OutputPins.ToArray()[0].Connect(filter0_0.InputPins.First());
-			graph.AddFilter(filter0_0);
-
-			var filter0_1 = (Filter) assembly.CreateInstance(BlurName);
-			Debug.Assert(filter0_1 != null);
-			filter0_0.OutputPins.First().Connect(filter0_1.InputPins.First());
-			graph.AddFilter(filter0_1);
-
-			var filter0_2 = (Filter) assembly.CreateInstance(BlurName);
-			Debug.Assert(filter0_2 != null);
-			filter0_1.OutputPins.First().Connect(filter0_2.InputPins.First());
-			graph.AddFilter(filter0_2);
-
-			var filter1 = (Filter) assembly.CreateInstance(BlurName);
-			Debug.Assert(filter1 != null);
-			clone.OutputPins.ToArray()[1].Connect(filter1.InputPins.First());
-			graph.AddFilter(filter1);
-
-			var destFileImage3 = new DestFileImage();
-			destFileImage3.Properties.Values.First().Value = @"output3.png";
-			filter0_2.OutputPins.First().Connect(destFileImage3.InputPins.First());
-			graph.AddFilter(destFileImage3);
-
-			var destFileImage1 = new DestFileImage();
-			destFileImage1.Properties.Values.First().Value = @"output1.png";
-			filter1.OutputPins.First().Connect(destFileImage1.InputPins.First());
-			graph.AddFilter(destFileImage1);
-			return graph;
+			foreach (Filter filter in graphControl1.GraphBundle.Graph.Filters)
+			{
+				Filter localFilter = filter;
+				ThreadPool.QueueUserWorkItem(o => localFilter.Process());
+			}
 		}
 	}
 }
