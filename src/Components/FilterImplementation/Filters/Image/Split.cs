4ff269daeec27d69225c0748b73f0c96c7cc6a03
Alexander Kholodovitch <kholodovitch@gmail.com>
2012-04-12 16:51:23 +0000

    add filter for splitting image by channels

---
+++ src/Components/FilterImplementation/Filters/Image/Split.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using DataStructures;
+using DataStructures.Enums;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using FilterImplementation.Base;
+
+namespace FilterImplementation.Filters.Image
+{
+	internal class Split : Filter
+	{
+		private readonly OutputPin _outputR;
+		private readonly OutputPin _outputG;
+		private readonly OutputPin _outputB;
+		private InputPin _input;
+
+		public Split()
+		{
+			_input = new InputPin("Input", PinMediaType.Image);
+			_outputB = new OutputPin("B", PinMediaType.Image);
+			_outputG = new OutputPin("G", PinMediaType.Image);
+			_outputR = new OutputPin("R", PinMediaType.Image);
+			AddPin(_input);
+			AddPin(_outputR);
+			AddPin(_outputG);
+			AddPin(_outputB);
+		}
+
+		#region Overrides of Filter
+
+		public override Guid TypeGuid
+		{
+			get { return new Guid("E67796A6-F73A-4E64-B4D7-766B612BE02B"); }
+		}
+
+		public override void Process()
+		{
+			FireProcessingStateChanged(ProcessingState.Started);
+			var frame = (IImage)_input.GetData();
+
+			if (frame is Image<Bgr, byte>)
+			{
+				var t = (Image<Bgr, byte>)frame;
+				var channels = t.Split();
+				_outputB.SetData(channels[0]);
+				_outputG.SetData(channels[1]);
+				_outputR.SetData(channels[2]);
+			}
+			else if (frame is Image<Rgb, byte>)
+			{
+				var t = (Image<Rgb, byte>)frame;
+				var channels = t.Split();
+				_outputR.SetData(channels[0]);
+				_outputG.SetData(channels[1]);
+				_outputB.SetData(channels[2]);
+			}
+
+			FireProcessingStateChanged(ProcessingState.Finished);
+		}
+
+		#endregion
+	}
+}
