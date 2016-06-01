2cb124a19f75fe61118bb149e4a778f637490be9
Alexander Kholodovitch <kholodovitch@gmail.com>
2016-06-01 18:20:11 +0300

    add empty class for implement matix of ouptut screens

---
+++ src/Components/FilterImplementation/Destination/WinFormsMatrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FilterImplementation.Base;
+
+namespace FilterImplementation.Destination
+{
+	internal class WinFormsMatrix : Filter
+	{
+		public override Guid TypeGuid
+		{
+			get { return new Guid("ECF4A9F5-3C92-4A4D-8D1D-56DF184A5C8E"); }
+		}
+
+		public override void Process()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
